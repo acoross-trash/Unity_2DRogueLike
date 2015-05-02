45404529b1b3de95e371b72317d86a5c00463dc6
acoross <acoross@gmail.com>
2015-05-02 14:52:00 +0900

    tutorial study

---
+++ 2D_Rogue_Like/Assets/Scripts/Loader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour 
+{
+	public GameObject gameManager;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		// GameManager 싱글턴 인스턴스가 아직 null 이면 gameManager 를 Instantiate
+		// 	여기서 gameManager 는 UnityEngine 을 통해 gamaManager prefab 과 연결되어 있다.
+		// 		즉, Instantiate 를 통해 gameManager prefab 이 GameObject 가 되는 것.
+		if (GameManager.instance == null)
+		{
+			Instantiate(gameManager);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
